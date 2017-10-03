07aaa0209f00f5c0f23fa3c7a556fa53f880b0da
Owain <owaine1@live.com>
2017-10-03 12:12:13 +0800

    The last time I saw him he was walking down Lover's Lane holding his own hand. 		-- Fred Allen

---
+++ js/javascript.js
@@ -0,0 +1,64 @@
+console.log('Hello are you here?');
+
+// vars go here!
+
+$(document).ready();
+
+function setup(){
+  console.log('Inside setup');
+}
+function randomDisplayCards(){
+  console.log('Inside randomDisplayCards');
+}
+/* This function might change, unsure of exact game yet
+function randomPairsStart(){
+  console.log('Inside randomPairsStart');
+}
+*/
+function playerTurnTimer(){
+  console.log('Inside playerTurnTimer');
+}
+
+function totalPlayerTimeTaken(){
+  console.log('Inside totalPlayerTimeTaken');
+}
+
+function rightCardsPicked(){
+  console.log('Inside rightCardsPicked');
+  // if / else statement cards picked display: "right cards = Well done!", Do something
+  // add point to players score
+  // if wrong cards picked, display: "wrong cards = sorry try again", Go to other player
+  //better put: win or lose
+}
+
+function scoreCounter(){
+  console.log('Inside scoreCounter');
+}
+
+// Canvas element of counter flashing and twisting and being added to player score.
+// Related to rightCardsPicked function.
+// Still need counter in main javaScript, not displayed but in memory
+
+
+function switchPlayers(){
+  console.log('Inside switchPlayers');
+}
+
+
+
+function concedeGame(){
+  console.log('Inside concedeGame');
+}
+function finalScoreCounter(){
+  console.log('Inside finalScoreCounter');
+}
+
+function endOfGame(){
+  console.log('Inside endOfGame');
+  // counter of amount 1st player to score 3
+  // add a "click to play again" flash-up in jQuery. jQuery is clicked, go to setup
+}
+
+function resetButton(){
+  console.log('Inside resetButton');
+}
