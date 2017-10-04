a6aee83a46c682618aae5caa9b8bb570603c6a00
Owain <owaine1@live.com>
2017-10-04 12:02:27 +0800

    F:	When into a room I plunge, I 	Sometimes find some VIOLET FUNGI. 	Then I linger, darkly brooding 	On the poison they're exuding. 		-- The Roguelet's ABC

--- js/javascript.js
+++ js/javascript.js
@@ -3,77 +3,83 @@ console.log('Hello here?');
 // vars go here!
 // see https://stackoverflow.com/questions/21648147/selecting-a-random-image
 // for suggestion of how to complete...
-/*
-imageArray[0] = 'backOfCard.svg';
-imageArray[1] = 'blackBox.svg';
-imageArray[2] = 'blackCircle.svg';
-imageArray[3] = 'blackCross.svg';
-imageArray[4] = 'blackHex.svg';
-imageArray[5] = 'blackTri.svg';
-imageArray[6] = 'blueBox.svg';
-imageArray[7] = 'blueCircle.svg';
-imageArray[8] = 'blueCross.svg';
-imageArray[9] = 'blueHex.svg';
-imageArray[10] = 'blueTri.svg';
-imageArray[11] = 'greenBox.svg';
-imageArray[12] = 'greenCircle.svg';
-imageArray[13] = 'greenCross.svg';
-imageArray[14] = 'greenHex.svg';
-imageArray[15] = 'greenTri.svg';
-imageArray[16] = 'orangeBox.svg';
-imageArray[17] = 'orangeCircle.svg';
-imageArray[18] = 'orangeCross.svg';
-imageArray[19] = 'orangeHex.svg';
-imageArray[20] = 'orangeTri.svg';
-imageArray[21] = 'purpleBox.svg';
-imageArray[22] = 'purpleCircle.svg';
-imageArray[23] = 'purpleCross.svg';
-imageArray[24] = 'purpleHex.svg';
-imageArray[25] = 'purpleTri.svg';
-imageArray[26] = 'redBox.svg';
-imageArray[27] = 'redBoxCircle.svg';
-imageArray[28] = 'redCross.svg';
-imageArray[29] = 'redHex.svg';
-imageArray[30] = 'redTri.svg';
-imageArray[31] = 'yellowBox.svg';
-imageArray[32] = 'yellowCircle.svg';
-imageArray[33] = 'yellowCross.svg';
-imageArray[34] = 'yellowHex.svg';
-imageArray[35] = 'yellowTri.svg';
-*/
+
+// N.B. 'backOfCard.svg' missing because default hidden card
+imageArray = new Array();
+imageArray[0] = 'images/blackBox.svg';
+imageArray[1] = 'images/blackCircle.svg';
+imageArray[2] = 'images/blackCross.svg';
+imageArray[3] = 'images/blackHex.svg';
+imageArray[4] = 'images/blackTri.svg';
+imageArray[5] = 'images/blueBox.svg';
+imageArray[6] = 'images/blueCircle.svg';
+imageArray[7] = 'images/blueCross.svg';
+imageArray[8] = 'images/blueHex.svg';
+imageArray[9] = 'images/blueTri.svg';
+imageArray[10] = 'images/greenBox.svg';
+imageArray[11] = 'images/greenCircle.svg';
+imageArray[12] = 'images/greenCross.svg';
+imageArray[13] = 'images/greenHex.svg';
+imageArray[14] = 'images/greenTri.svg';
+imageArray[15] = 'images/orangeBox.svg';
+imageArray[16] = 'images/orangeCircle.svg';
+imageArray[17] = 'images/orangeCross.svg';
+imageArray[18] = 'images/orangeHex.svg';
+imageArray[19] = 'images/orangeTri.svg';
+imageArray[20] = 'images/purpleBox.svg';
+imageArray[21] = 'images/purpleCircle.svg';
+imageArray[22] = 'images/purpleCross.svg';
+imageArray[23] = 'images/purpleHex.svg';
+imageArray[24] = 'images/purpleTri.svg';
+imageArray[25] = 'images/redBox.svg';
+imageArray[26] = 'images/redBoxCircle.svg';
+imageArray[27] = 'images/redCross.svg';
+imageArray[28] = 'images/redHex.svg';
+imageArray[29] = 'images/redTri.svg';
+imageArray[30] = 'images/yellowBox.svg';
+imageArray[31] = 'images/yellowCircle.svg';
+imageArray[32] = 'images/yellowCross.svg';
+imageArray[33] = 'images/yellowHex.svg';
+imageArray[34] = 'images/yellowTri.svg';
+
 $(document).ready(setup);
 
 // checked so far working to here*
 function setup(){
   console.log('Setup loaded');
-
-  $('#seconds_counter').click(loading);
+  // backOfCard.svg needs to be default setup tile
   $('[id^="card"]').attr("src", "images/backOfCard.svg");
   // $('#card1').attr("src", "images/blackHex.svg");
-  // $('#card10').attr("src", "images/blackCircle.svg");
 
-  // backOfCard.svg needs to be default setup tile
   console.log($('#backOfCard'));
+  return(randomDisplayCards);
+  $('#seconds_counter').click(loading);
 
 }
 //  to here*
 
-//document.getElementById('#box1').textContent = 'Check!';
 function randomDisplayCards() {
   console.log('Inside randomDisplayCards');
+  $('#card1').attr("src", "images/blackHex.svg");
   // some way that js will select random images for the grid
-    // put 3 random imgs in class col 1 + 1 constant
+  // TEST display 16 randomDisplayCards
+  // getRandomImage(imageArray, "");
+  // console.log(imageArray);
 
+  // var randImage = ImageArray[Math.floor(Math.random() * 16)];
+  // $('^#card').attr("src", randImage);
+  // console.log(randImage);
+
+  // this is a for loop?
+    // put 3 random imgs in class col 1 + 1 constant
     // put 3 random imgs in class col 2 + 1 constant
     // put 3 random imgs in class col 3 + 1 constant
     // put 3 random imgs in class col 4 + 1 constant
-      // this is a for loop
 //  something like https://stackoverflow.com/questions/21648147/selecting-a-random-image
   //  of 16 tiles, 12 are different, 4 are the same
   // the 4 appear 1 time in every column
   // var num = Math.floor(Math.random() * ?);
   // per column 1 constant, 3 randoms; same for the other 4 columns
-
 }
 
 
@@ -132,7 +138,7 @@ function concedeGameButton(){
 
 function finalScoreCounter(){
   console.log('Inside finalScoreCounter');
-  // adds / holds final scores of each player and caluculates totalPlayerTimeTaken
+  // adds / holds final scores of each player and calculates totalPlayerTimeTaken
   // calculate players counters and factor in amount of time totalPlayerTimeTaken
   // very simmial / related / combined / callback? to endOfGame
 }
